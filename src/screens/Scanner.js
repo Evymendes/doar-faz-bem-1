96feba9a4db111580613c51d993072094c9f2c8b
mymrtt <mymrtt@gmail.com>
2020-07-14 17:36:37 -0300

    adding modal with isbn code

--- src/screens/Scanner.js
+++ src/screens/Scanner.js
@@ -1,5 +1,4 @@
-// Libs
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Quagga from 'quagga';
 
@@ -50,83 +49,128 @@ const ScanMarker = styled.div`
 const MarkerDesc = styled.p`
 	font-size: 1rem;
 	color: #fff;
+	font-weight: 600;
 `;
 
-function Scanner() {
-	let scannerAttemps = 0;
+const ContainerModalDetails = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	min-width: 100%;
+	min-height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
 
-	const onDetected = (result) => {
-		Quagga.offDetected(onDetected);
+const ContainerModal = styled.div`
+	width: 25%;
+	height: 25%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background: #0000ff;
+`;
 
-		console.log('result', result);
+const ContentModal = styled.div`
+	flex: 1;
+`;
 
-		const isbn = result.codeResult.code;
+class Scanner extends Component {
+  state = {
+  	modalOpen: false,
+  }
 
-		if (validateIsbn(isbn)) {
-			alert(`ISBN válido ${isbn}`);
-		}
+  componentDidMount() {
+  	if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia) {
+  		Quagga.init({
+  			inputStream: {
+  				name: 'Live',
+  				type: 'LiveStream',
+  				target: document.querySelector('#video'),
+  				constraints: {
+  					facingMode: 'environment',
+  				},
+  			},
+  			numOfWorkers: 1,
+  			locate: true,
+  			decoder: {
+  				readers: ['code_128_reader', 'ean_reader', 'ean_8_reader',
+  					'code_39_reader', 'code_39_vin_reader', 'codabar_reader',
+  					'upc_reader', 'upc_e_reader', 'i2of5_reader',
+  					'2of5_reader', 'code_93_reader',
+  				],
+  			},
+  		}, (err) => {
+  			if (err) {
+  				console.log(err);
+  				alert('erro ao abrir a camera do dispositivo');
+  				return;
+  			}
+  			Quagga.start();
+  		});
+  		Quagga.onDetected(this.onDetected);
+  	}
+  }
 
-		if (scannerAttemps >= 5) {
-			alert('não foi possível ler o código. tente novamente');
-		}
+  onDetected = (result) => {
+  	let scannerAttemps = 0;
+  	Quagga.offDetected(this.onDetected);
 
-		scannerAttemps++;
-		Quagga.onDetected(onDetected);
+  	console.log('result', result);
 
-		console.log('isbn', isbn);
-	};
+  	const isbn = result.codeResult.code;
+  	// const isbn = '7898927111014';
 
-	useEffect(() => {
-		if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia) {
-			Quagga.init(
-				{
-					inputStream: {
-						name: 'Live',
-						type: 'LiveStream',
-						target: document.querySelector('#video'),
-						constraints: {
-							facingMode: 'environment',
-						},
-					},
-					numOfWorkers: 1,
-					locate: true,
-					decoder: {
-						readers: ['code_128_reader', 'ean_reader', 'ean_8_reader',
-							'code_39_reader', 'code_39_vin_reader', 'codabar_reader',
-							'upc_reader', 'upc_e_reader', 'i2of5_reader',
-							'2of5_reader', 'code_93_reader',
-						],
-					},
-				},
-				(err) => {
-					if (err) {
-						console.log(err);
-						alert('erro ao abrir a camera do dispositivo');
-						return;
-					}
-					Quagga.start();
-				},
-			);
-			Quagga.onDetected(onDetected);
-		}
-	}, []);
+  	if (validateIsbn(isbn)) {
+  		this.setState({
+				modalOpen: true,
+				isbnCode: isbn,
+  		});
 
-	return (
-		<>
-			<Video id="video" />
-			<Container>
-				<ScanMarker>
-					<img
-						src={Logo}
-						alt="logo"
-						width="260"
-						height="260"
-					/>
-					<MarkerDesc>Aponte para o codigo de barras do medicamento.</MarkerDesc>
-				</ScanMarker>
-			</Container>
-		</>
+  		alert(`ISBN válido ${isbn}`);
+  	} else if (scannerAttemps >= 5) {
+  		alert('não foi possível ler o código. tente novamente');
+  	}
+
+  	scannerAttemps++;
+  	Quagga.onDetected(this.onDetected);
+  }
+
+	renderModal = () => (
+		<ContainerModal>
+			<ContentModal>
+				<p>nome do remedio</p>
+				<p>desc do remedio</p>
+				<p>codigo do remédio {this.state.isbnCode}</p>
+				<button>+</button>
+			</ContentModal>
+		</ContainerModal>
 	);
+
+	render() {
+  	return (
+  		<>
+  			<Video id="video" />
+  			<Container>
+  				<ScanMarker>
+  					<img
+  						src={Logo}
+  						alt="logo"
+  						width="260"
+  						height="260"
+  					/>
+  					<MarkerDesc>Aponte para o codigo de barras do medicamento.</MarkerDesc>
+  				</ScanMarker>
+  			</Container>
+				<ContainerModalDetails>
+					{this.state.modalOpen && this.renderModal()}
+				</ContainerModalDetails>
+  		</>
+  	);
+	}
 }
 
 export default Scanner;
