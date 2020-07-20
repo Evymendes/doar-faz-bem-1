e2e038969f0bd7b242eeb48507b825d8b2ed68e4
mymrtt <mymrtt@gmail.com>
2020-07-20 14:56:15 -0300

    Updates

---
+++ precache-manifest.b3eace9f4bb99ce08b28eb8f4a87bc7d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6516d32c8e98b5d74e279e5510fd410a",
+    "url": "/doar-faz-bem/index.html"
+  },
+  {
+    "revision": "2cc835af39ca59803448",
+    "url": "/doar-faz-bem/static/css/main.ec7993d7.chunk.css"
+  },
+  {
+    "revision": "72194fc3a5fce99a3875",
+    "url": "/doar-faz-bem/static/js/2.6680a4fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/doar-faz-bem/static/js/2.6680a4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc835af39ca59803448",
+    "url": "/doar-faz-bem/static/js/main.a63836e9.chunk.js"
+  },
+  {
+    "revision": "13a75551b523f6a0d012",
+    "url": "/doar-faz-bem/static/js/runtime-main.9080439c.js"
+  },
+  {
+    "revision": "3332ace6f5404f3cbb134fd623a017c8",
+    "url": "/doar-faz-bem/static/media/fechar.3332ace6.svg"
+  },
+  {
+    "revision": "5c0e88791db294f5d9675cd8785369c3",
+    "url": "/doar-faz-bem/static/media/headerbg.5c0e8879.jpeg"
+  },
+  {
+    "revision": "2f17b18c99d08baa9bdec9f3dd602568",
+    "url": "/doar-faz-bem/static/media/markerIcon.2f17b18c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94adceae9b9f5a7252422c35575da12b.js"
+  "/doar-faz-bem/precache-manifest.b3eace9f4bb99ce08b28eb8f4a87bc7d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doar-faz-bem/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
