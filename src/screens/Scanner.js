9ad25cb135f9ebd97447773bddb6d3949d0cc46f
mymrtt <mymrtt@gmail.com>
2020-07-30 15:17:55 -0300

    cleaning code

--- src/screens/Scanner.js
+++ src/screens/Scanner.js
@@ -141,52 +141,6 @@ const InputBarCode = styled.input`
 	background: #EDEDED;
 `;
 
-const Button = styled.button`
-	margin-bottom: ${(props) => (props.addInfo && '2rem')};
-	width: 20rem;
-	height: 3.5rem;
-	text-align: center;
-	color: #fff;
-	font-size: 1rem;
-	font-family: 'Overpass', Bold;
-	font-weight: 800;
-	text-transform: ${(props) => (props.addInfo ? 'null' : 'uppercase')};
-	text-decoration: none;
-	border: none;
-	border-radius: 50px;
-	box-shadow: 2px 2px 2px #888888;
-	background: ${(props) => (props.addInfo ? '#D8998A' : '#49E5D6')};
-	cursor: pointer;
-`;
-
-const ModalDetails = styled.div`
-	flex: 1;
-	height: 100vh;
-	background: #fff;
-`;
-
-const ContentModalDetails = styled.div`
-	margin-top: 3rem;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-`;
-
-const TextModalDetails = styled.h2`
-	margin-bottom: ${(props) => (props.title && '1rem')};
-	font-size: ${(props) => (props.title ? '1.5rem' : '1.2rem')};
-	font-weight: 800;
-	font-family: 'Overpass', Bold;
-`;
-
-const ContainerIbsnCode = styled.div`
-	margin-bottom: 5rem;
-	padding: 3rem;
-	border: 3px solid #49E5D6;
-	border-radius: 10px;
-`;
-
 const ErrorMessage = styled.span`
 	margin-top: 1rem;
   color: red;
