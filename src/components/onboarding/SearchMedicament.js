b21981aa4a7a28ac82741c20ae339f33658d07a7
mymrtt <mymrtt@gmail.com>
2020-08-21 11:03:53 -0300

    adding search medicament modal

---
+++ src/components/onboarding/SearchMedicament.js
@@ -0,0 +1,19 @@
+// Libs
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class SearchMedicament extends Component {
+	state = {
+
+	}
+
+	render() {
+		return (
+			<div>
+				a
+			</div>
+		)
+	}
+}
+
+export default SearchMedicament;
