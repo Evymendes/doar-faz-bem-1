3e303cc0bbc78a7215c8ffb5019791d88bfebb4b
mymrtt <mymrtt@gmail.com>
2020-08-21 14:50:05 -0300

    adding warrings messagens to scanner screen and changing the decoder readers

---
+++ src/components/WarringModal.js
@@ -0,0 +1,77 @@
+// Libs
+import React from 'react';
+import styled from 'styled-components';
+
+// Components
+import DefaultButton from './DefaultButton';
+
+// Styles
+const Overlay = styled.div`
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: rgba(196, 196, 196, 0.3);
+	z-index: 5;
+`;
+
+const Container = styled.div`
+	padding: 2rem;
+	max-width: 33%;
+	border-radius: 6px;
+	background: #fff;
+
+	@media(max-width: 667px) and (orientation: landscape) {
+		padding: 1rem;
+		max-width: 75%;
+	}
+`;
+
+const Text = styled.p`
+	padding-bottom: ${(props) => (props.title ? '1.5rem' : '1rem')};
+	color: ${(props) => (props.title ? '#D8998A' : '#404040')};
+	font-size: ${(props) => (props.title ? '1.3rem' : '.95rem')};
+	font-family: "Overpass", Regular;
+	font-weight: ${(props) => (props.title ? '800' : '400')};
+`;
+
+const ContainerButton = styled.div`
+	margin-top: .8rem;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const WarringModal = (props) => (
+	<Overlay>
+		<Container>
+			<Text title>Mensagem de Incompatibilidade</Text>
+			<Text>{props.firsText}</Text>
+			<Text>{props.secText}</Text>
+			<ContainerButton>
+				<DefaultButton
+					margin='0'
+					background= '#D8998A'
+					backgroundHover='#ce9385'
+					handleClick={props.modalCloseWarring}
+					text={'Ok'}
+				/>
+			</ContainerButton>
+		</Container>
+	</Overlay>
+);
+
+WarringModal.defaultProps = {
+	text: 'Hello World',
+	margin: '1rem 0',
+	color: '#c7c7c7',
+	background: '#fff',
+	border: 'none',
+};
+
+export default WarringModal;
--- src/screens/Scanner.js
+++ src/screens/Scanner.js
@@ -10,6 +10,7 @@ import { validateIsbn } from '../services/barcode';
 import Header from '../components/Header';
 import ExtractedInf from '../components/ExtractedInf';
 import DefaultButton from '../components/DefaultButton';
+import WarringModal from '../components/WarringModal';
 
 // Images
 import MarkerIcon from '../assets/markerIcon.svg';
@@ -203,6 +204,9 @@ class Scanner extends Component {
 		valueCode: '',
 		error: null,
 		pressed: false,
+		// mobLandscape: (window.matchMedia('(max-width: 667px) and (orientation: landscape)').matches),
+		// widthDesk: (window.matchMedia('(min-width: 1023px)').matches),
+		modalCloseWarring: false,
 	}
 
 	componentDidMount() {
@@ -219,11 +223,12 @@ class Scanner extends Component {
 				numOfWorkers: 1,
 				locate: true,
 				decoder: {
-					readers: ['code_128_reader', 'ean_reader', 'ean_8_reader',
-						'code_39_reader', 'code_39_vin_reader', 'codabar_reader',
-						'upc_reader', 'upc_e_reader', 'i2of5_reader',
-						'2of5_reader', 'code_93_reader',
-					],
+					// readers: ['code_128_reader', 'ean_reader', 'ean_8_reader',
+					// 	'code_39_reader', 'code_39_vin_reader', 'codabar_reader',
+					// 	'upc_reader', 'upc_e_reader', 'i2of5_reader',
+					// 	'2of5_reader', 'code_93_reader',
+					// ],
+					readers: ['ean_reader', 'ean_8_reader'],
 				},
 			}, (error) => {
 				if (error) {
@@ -341,6 +346,12 @@ class Scanner extends Component {
 		});
 	}
 
+	handleCloseWarringModal = () => {
+		this.setState({
+			modalCloseWarring: true,
+		});
+	}
+
 	renderModalBarCode = () => (
 		<ContainerModalBoilerPlate
 			display={this.state.modalOpenBarCode}
@@ -377,18 +388,27 @@ class Scanner extends Component {
 
 	render() {
 		const {
-			modalOpenDetails, modalOpenBarCode, modalOpenLoading,
+			modalOpenDetails, modalOpenBarCode, modalOpenLoading, modalCloseWarring,
 		} = this.state;
 
 		const mobLandscape = (window.matchMedia('(max-width: 667px) and (orientation: landscape)').matches);
-
-		!modalOpenBarCode && mobLandscape && (
-			alert('Não é possível efetuar a leitura do código de barras com a tela na horizontal. Vire-a, e tente novamente!')
-		);
+		const widthDesk = (window.matchMedia('(min-width: 1023px)').matches);
 
 		return (
 			<>
 				<Video id="video" />
+				{widthDesk && !modalCloseWarring && <WarringModal
+					firsText='Não é possível efetuar a leitura do código de barras pelo desktop.'
+					secText='Utilize um smartphone, e tente novamente.'
+					modalCloseWarring={this.handleCloseWarringModal}
+				/>
+				}
+				{!modalOpenBarCode && !modalCloseWarring && mobLandscape && <WarringModal
+					firsText='Não é possível efetuar a leitura do código de barras com a tela na horizontal.'
+					secText='Vire-a, e tente novamente.'
+					modalCloseWarring={this.handleCloseWarringModal}
+				/>
+				}
 				<ButtonBack onClick={this.handleBackScreen}>
 					<img src={BackIcon} alt="Voltar" />
 				</ButtonBack>
